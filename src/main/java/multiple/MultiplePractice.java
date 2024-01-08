5b98523e410ea62de51f03b13bcbd17bd3f73af0
zzooong2 <zzooong2@naver.com>
2024-01-08 21:49:09 +0900

    코딩테스트 공부

--- src/main/java/multiple/MultiplePractice.java
+++ src/main/java/multiple/MultiplePractice.java
@@ -4,11 +4,11 @@ public class MultiplePractice {
     public static void main(String[] args) {
         System.out.println("[구구단 출력]");
 
-        for(int i=1; i<10; i++){
+        for(int i=1; i<=9; i++){
             System.out.println();
             System.out.println("<" + i + "단>");
 
-            for(int j=1; j<10; j++){
+            for(int j=1; j<=9; j++){
                 System.out.println(i + "x" + j + "=" + i*j);
             }
         }
