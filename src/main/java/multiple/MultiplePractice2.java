cc9915c843923e3c43cfe751c010dabefb84bd67
zzooong2 <zzooong2@naver.com>
2024-01-17 18:17:30 +0900

    코딩테스트 공부

--- src/main/java/multiple/MultiplePractice2.java
+++ src/main/java/multiple/MultiplePractice2.java
@@ -4,11 +4,11 @@ public class MultiplePractice2 {
     public static void main(String[] args) {
         int count = 0;
 
-        System.out.println("5의 배수를 제외한 3단 출력");
+        System.out.println("5의 배수를 제외한 4단 출력");
 
         for(int i=1; i<10; i++){
-            if(((3*i)%5!=0)){
-                System.out.println(3 + "x" + i + "=" + 3*i);
+            if(((4*i)%5!=0)){
+                System.out.println(4 + "x" + i + "=" + 4*i);
             }
             count ++;
         }
