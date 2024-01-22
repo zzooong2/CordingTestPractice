a38a0d00708be44b950ff8afaac41cebd91c050b
zzooong2 <zzooong2@naver.com>
2024-01-22 20:11:16 +0900

    코딩테스트 공부

--- src/main/java/multiple/MultiplePractice.java
+++ src/main/java/multiple/MultiplePractice.java
@@ -5,12 +5,11 @@ public class MultiplePractice {
         System.out.println("[구구단 출력]");
 
         for(int i=1; i<=9; i++){
-            System.out.println();
-            System.out.println("<" + i + "단>");
-
+            System.out.println("<" + i + "단" + ">");
             for(int j=1; j<=9; j++){
                 System.out.println(i + "x" + j + "=" + i*j);
             }
+            System.out.println();
         }
     }
 }
--- src/main/java/textmove/Move.java
+++ src/main/java/textmove/Move.java
@@ -2,9 +2,9 @@ package textmove;
 
 public class Move {
     public static void main(String[] args) {
-        for (int i=0; i<6; i++){
-            for (int j=0; j<6; j++){
-                if (i - j == 0 ){
+        for(int i=1; i<7; i++){
+            for(int j=1; j<7; j++){
+                if(i-j==0){
                     System.out.print(">");
                 } else {
                     System.out.print("-");
@@ -12,10 +12,9 @@ public class Move {
             }
             System.out.println();
         }
-
-        for (int i=0; i<6; i++){
-            for (int j=0; j<6; j++){
-                if (i + j == 5) {
+        for(int i=1; i<7; i++){
+            for(int j=1; j<7; j++){
+                if(i+j==7){
                     System.out.print("<");
                 } else {
                     System.out.print("-");
