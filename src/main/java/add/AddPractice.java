38893a8b404c0e5b2e8d404e1aa3a6fb99ca0021
zzooong2 <zzooong2@naver.com>
2024-01-27 16:50:48 +0900

    코딩테스트 공부

--- src/main/java/add/AddPractice.java
+++ src/main/java/add/AddPractice.java
@@ -4,11 +4,11 @@ public class AddPractice {
     public static void main(String[] args) {
         int sum = 0 ;
 
-        for(int i=1; i<10; i++){
+        for(int i=1; i<100; i++){
             sum += i;
         }
-        
-        System.out.println(" 1부터 9까지의 합 = " + sum);
+
+        System.out.println(" 1부터 99까지의 합 = " + sum);
     }
 
 }
