c50ec2b773d57455b96fb3a83a74f3b1b9d8f741
zzooong2 <zzooong2@naver.com>
2024-01-29 20:18:25 +0900

    코딩테스트 공부

--- src/main/java/multiple/MultiplePractice2.java
+++ src/main/java/multiple/MultiplePractice2.java
@@ -2,16 +2,15 @@ package multiple;
 
 public class MultiplePractice2 {
     public static void main(String[] args) {
-        int count = 0;
 
-        System.out.println("7의 배수를 제외한 4단 출력");
-
+        System.out.println("7의 배수를 제외한 5단 출력");
         for(int i=1; i<10; i++){
-            if(((4*i)%7!=0)){
-                System.out.println(4 + "x" + i + "=" + 4*i);
+            if(((5 * i) % 7)!=0){
+                System.out.println(5 + " x " + i + " = " + 5*i);
             }
-            count ++;
         }
-        System.out.println("총 개수 = " + count);
     }
 }
+
+
+
